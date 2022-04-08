5c758f7d8ed6d421a2bb3d652c4758e65c127647
StevenGuiry <s.guiry@hotmail.com>
2022-04-08 13:00:04 +0100

    Docker compose container

---
+++ app/create_app.py
@@ -0,0 +1,29 @@
+from crypt import methods
+from flask import Flask, request
+#from flask_cors import CORS
+import numpy as np
+import pickle
+
+model = None
+
+def create_app():
+    app = Flask(__name__)
+    #CORS(app)
+
+    with open('xgb_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+
+    @app.route("/")
+    def index():
+        return "hello world"
+
+    @app.route('/predict', methods=['POST'])
+    def get_prediction():
+        if request.method == 'POST':
+            data = request.get_json()
+            data = np.array(data)[np.newaxis, :]
+            prediction = model.predict(data)
+        return str(prediction[0])
+    
+    return app
