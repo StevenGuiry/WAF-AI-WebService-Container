8c0f05e19a90516e019d87fe7c946a32a85fa608
46968144 <46968144+StevenGuiry@users.noreply.github.com>
2022-04-28 15:44:02 +0100

    Added more features

--- app/create_app.py
+++ app/create_app.py
@@ -5,8 +5,10 @@ import numpy as np
 import pickle
 import logging
 import json
+import urllib.parse
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+words = ['SLEEP', 'DROP', 'UNION', 'SELECT', 'WAITFOR', 'DELAY', 'ORDER BY', 'GROUP BY', 'DELETE','OR', 'AND', 'WHERE']
 
 model = None
 
@@ -25,16 +27,29 @@ def create_app():
     @app.route('/predict', methods=['POST'])
     def get_prediction():
         if request.method == 'POST':
+            
+            sql_words = 0
+            # Parse request 
             data = request.get_json()
             data = json.dumps(data)
+            # Decode any URL encoding
+            data = urllib.parse.unquote_plus(data)
+            # Count the features
             single_q = data.count("'")
             double_q = data.count("\"")
+            dash = data.count("-")
+            brace = data.count("(") + data.count(")")
             slash = data.count("/")
             dot = data.count(".")
+            asterik = data.count("*")
+            for word in words:
+                sql_words += data.count(word)
             if single_q == 0 & double_q > 2:
                 no_quote = 1
             else:
                 no_quote = 0
+
+            # Send features to model to get prediction
             data = [single_q, dot, slash, no_quote]
             app.logger.info(str(data))
             data = np.array(data)[np.newaxis, :]
